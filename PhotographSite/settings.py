4f1635611611f15c21424f960c346cb56deadde3
XeonStaf <alekseiasta@gmail.com>
2021-08-01 00:13:58 +0700

    Ready for deploy FIXED

--- PhotographSite/settings.py
+++ PhotographSite/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
+import django_heroku
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -172,3 +173,4 @@ with open('PhotographSite/secret.txt', 'r') as f:
 VK_BOT_TOKEN = data['VK_BOT_TOKEN']
 SECRET_KEY = data['SECRET_KEY']
 DEBUG = data['DEBUG']
+django_heroku.settings(locals())
